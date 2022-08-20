db468f42a425d813a318f735516c06986eacd417
Astikk <lusiferr2001@gmail.com>
2022-08-20 11:21:00 +0530

    validate form (Textfield)

--- lib/main.dart
+++ lib/main.dart
@@ -15,34 +15,85 @@ class LoginPage extends StatefulWidget {
 
 
 class _LoginPageState extends State<LoginPage> {
+
+  final formKey=GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Stack(
-          children: [
-            Container(
-              color: Colors.blue[300],
-            ),
-            Container(
-              padding: EdgeInsets.all(20),
-              alignment: Alignment.center,
-              child: Column(
-                children: [
-                  Text('Login',style: TextStyle(
-                      fontSize: 35),
-                  ),
-              ClipRRect(
-                borderRadius: BorderRadius.circular(80),
-                child: Image.asset(
-                  'images/login.png',height: 150,
-                ),
-              )
-                ],
+        body: Form(
+          key: formKey,
+          child: Stack(
+
+            children: [
+
+              Container(
+                color: Colors.blue[200],
               ),
 
-            )
-          ],
+              Container(
+
+                width: double.infinity,
+                padding: EdgeInsets.all(20),
+
+                child: Column(
+
+                  children: [
+
+                    Text('Login',
+                      style: TextStyle(
+                        fontSize: 35),
+                    ),
+
+                ClipRRect(
+
+                  borderRadius: BorderRadius.circular(80),
+                  child: Image.asset(
+                    'images/login.png',height: 150,
+                  ),
+                ),
+
+                    // For text Validation
+                    TextFormField(
+                      //what we wrote in to input field
+                      validator: (value){
+                        print('${value}');
+                        if(value!.length !=10){
+                          return 'Number is not correct';
+                        }
+                        else{
+                          return null;
+                        }
+                      },
+                      decoration: InputDecoration(
+                        hintText: 'Number',
+                        fillColor: Colors.white,
+                        filled: true,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                      keyboardType: TextInputType.number,
+                    ),
+
+                    ElevatedButton(
+                        onPressed: (){
+                            if(formKey.currentState!.validate()){
+                              print('Hurreee');
+                            }
+                            else{
+                              print('Something is Wrong..');
+                            }
+                    },
+                        child: Text('Submit')
+                    )
+                  ],
+                ),
+
+              )
+            ],
+          ),
         ),
       ),
     );
